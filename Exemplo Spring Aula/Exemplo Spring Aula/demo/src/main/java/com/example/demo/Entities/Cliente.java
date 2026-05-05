3a4572f3ceefdad4bca2a603116f82d7e85cfcfe
Pedro Alvaro <fernandesfilhopedro@gmail.com>
2026-05-05 20:53:10 -0300

    DTO, Repository, Entities de cliente

---
+++ Exemplo Spring Aula/Exemplo Spring Aula/demo/src/main/java/com/example/demo/Entities/Cliente.java
@@ -0,0 +1,37 @@
+package com.example.demo.Entities;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Data
+@Entity
+@Table(name = "clientes")
+@Getter @Setter
+@NoArgsConstructor @AllArgsConstructor
+public class Cliente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nome;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String telefone;
+}
+
