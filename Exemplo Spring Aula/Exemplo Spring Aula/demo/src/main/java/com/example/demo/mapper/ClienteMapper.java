529f6583145d1afaf1d38337f636696ea954962f
Pedro Alvaro <fernandesfilhopedro@gmail.com>
2026-05-06 18:48:02 -0300

    Controller, Mapper e Service de cliente

---
+++ Exemplo Spring Aula/Exemplo Spring Aula/demo/src/main/java/com/example/demo/mapper/ClienteMapper.java
@@ -0,0 +1,21 @@
+package com.example.demo.mapper;
+
+import java.util.List;
+
+import org.mapstruct.Mapper;
+
+import com.example.demo.Entities.Cliente;
+import com.example.demo.Entities.Cliente;
+import com.example.demo.dto.ClienteDTO;
+import com.example.demo.dto.ClienteDTO;
+
+@Mapper(componentModel = "spring")
+public interface ClienteMapper {
+    
+
+    ClienteDTO toDTO(Cliente cliente);
+
+    Cliente toEntity(ClienteDTO clienteDTO);
+
+    List<ClienteDTO> toDTOList(List<Cliente> cliente);
+}
